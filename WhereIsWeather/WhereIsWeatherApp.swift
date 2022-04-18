1381fccc3fc48174185c3406a56f6cd4a21ce4b1
SwiftViktor <panafviktor@gmail.com>
2022-04-18 11:37:54 +0300

    Initial Commit

---
+++ WhereIsWeather/WhereIsWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhereIsWeatherApp.swift
+//  WhereIsWeather
+//
+//  Created by Viktor on 18.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct WhereIsWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
